070ff3a8a06d2f0e8f0268c74562677076da6d67
blrnw3 <ben.lee-rodgers@artsalliancemedia.com>
2013-10-03 16:42:13 +0100

    Hide the Subtasks section on the issue-view pages if the parent_task field is turned off (as it should be).

--- lib/aam_lifeguard/plugins/aam_ui_cleanup/lib/cleanup_hooks.rb
+++ lib/aam_lifeguard/plugins/aam_ui_cleanup/lib/cleanup_hooks.rb
@@ -1,5 +1,6 @@
 module CleanupHooks
   class CleanHooks < Redmine::Hook::ViewListener
-    render_on :view_issues_form_details_bottom, :partial => 'cleanup_hook'
+    render_on :view_issues_form_details_bottom, :partial => 'cleanup_form_hook'
+    render_on :view_issues_show_description_bottom, :partial => 'cleanup_show_hook'
   end
 end
