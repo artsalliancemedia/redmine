e19d28d4a79089b2662d7e08ec79fdb960849dba
blrnw3 <ben.lee-rodgers@artsalliancemedia.com>
2013-10-07 17:58:01 +0100

    Fixes for our patched version of Redmine.

--- lib/aam_lifeguard/plugins/redmine_category_tree/db/migrate/001_issue_category_acts_as_nested_set.rb
+++ lib/aam_lifeguard/plugins/redmine_category_tree/db/migrate/001_issue_category_acts_as_nested_set.rb
@@ -2,19 +2,18 @@ class IssueCategoryActsAsNestedSet < ActiveRecord::Migration
 
   def self.up
     add_column :issue_categories, :parent_id, :integer, :null => true
-	add_column :issue_categories, :lft, :integer, :null => true
-	add_column :issue_categories, :rgt, :integer, :null => true
+		add_column :issue_categories, :lft, :integer, :null => false, :default => '0'
+		add_column :issue_categories, :rgt, :integer, :null => false, :default => '0'
 
-	categories = IssueCategory.count
-	if categories > 0
-		IssueCategory.rebuild!
-	end
+		if IssueCategory.count > 0
+			IssueCategory.rebuild!
+		end
   end
 
   def self.down
-  	remove_column :issue_categories, :parent_id
-	remove_column :issue_categories, :lft
-	remove_column :issue_categories, :rgt
+		remove_column :issue_categories, :parent_id
+		remove_column :issue_categories, :lft
+		remove_column :issue_categories, :rgt
   end
 
 end
