5d83c37827bb42814e328e005d9b12c1835f5753
Danny Butler <danny.butler@artsalliancemedia.com>
2013-10-10 14:37:56 +0100

    Adding a pause list page
    Adding a new page to display a list of all of the previous (and current)
    pauses for the current issue. Button is located next to the
    pause/unpause button.

--- src/plugins/aam_sla/app/controllers/pauses_controller.rb
+++ src/plugins/aam_sla/app/controllers/pauses_controller.rb
@@ -1,5 +1,6 @@
 class PausesController < ApplicationController
-  before_filter :find_pause, only => [:edit, :destroy]
+  before_filter :find_pause, :only => [:edit, :destroy]
+  before_filter :find_issue, :only => [:show]
 
   def find_pause
     @pause = Pause.find(params[:id])
@@ -7,5 +8,9 @@ class PausesController < ApplicationController
 
   def create
     @pause = Pause.new(:issue_id => params[:issue_id], :start_date => DateTime.now)
+  end
+
+  def show
+    @pauses = Pause.all.select{|p| p.issue_id == @issue.id}
   end
 end
