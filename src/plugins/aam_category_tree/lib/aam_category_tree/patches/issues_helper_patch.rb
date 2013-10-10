e111b53df6160b1f1417585822dbeb264c100743
blrnw3 <ben.lee-rodgers@artsalliancemedia.com>
2013-10-10 15:49:53 +0100

    Bug fix for deleting a category and reassigning issues with that category causing 500 error on issues#show.

--- src/plugins/aam_category_tree/lib/aam_category_tree/patches/issues_helper_patch.rb
+++ src/plugins/aam_category_tree/lib/aam_category_tree/patches/issues_helper_patch.rb
@@ -7,10 +7,7 @@ module AamCategoryTree
 
 				base.class_eval do
 				  unloadable
-				  
 				  include AamCategoryTree::IssueCategoryHelper
-
-					alias_method_chain :find_name_by_reflection, :issue_categories unless method_defined?('find_name_by_reflection_without_issue_categories')
 				end
 			end
 
@@ -18,17 +15,6 @@ module AamCategoryTree
 			end
 
 			module InstanceMethods
-				def find_name_by_reflection_with_issue_categories(field, id)
-          unless id.present?
-            return nil
-          end
-					if field == 'category'
-					  category = IssueCategory.find(id)
-					  return render_issue_category_with_tree_inline(category)
-					else
-						return find_name_by_reflection_without_issue_categories(field, id)
-					end
-				end
 			end
 		end
 	end
