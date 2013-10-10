8ec63249218fca5b3b770105a9e86e6cc5b9b3f6
Danny Butler <danny.butler@artsalliancemedia.com>
2013-10-10 14:37:56 +0100

    Adding a pause list page
    Adding a new page to display a list of all of the previous (and current)
    pauses for the current issue. Button is located next to the
    pause/unpause button.

--- lib/aam_lifeguard/plugins/aam_sla/app/models/pause.rb
+++ lib/aam_lifeguard/plugins/aam_sla/app/models/pause.rb
@@ -1,7 +1,20 @@
 class Pause < ActiveRecord::Base
-	validates_presence_of :issue_id, :start_date
+	validates :issue_id, :start_date, :presence => true
 	belongs_to :issue
 
+  def start_date_string
+    date_to_s(self.start_date)
+  end
+
+  def end_date_string
+    self.end_date == nil ? l(:still_paused) : date_to_s(self.end_date)
+  end
+
+  def date_to_s(date)
+    time_zone = (User.current.time_zone.blank? ? Time.zone : User.current.time_zone)
+    date.in_time_zone(time_zone).strftime('%T %D')
+  end
+  
 	def active?
 		# Returns true if the issue is currently paused
 		self.end_date.blank? || self.end_date > DateTime.now.utc
--- lib/aam_lifeguard/plugins/aam_sla/config/routes.rb
+++ lib/aam_lifeguard/plugins/aam_sla/config/routes.rb
@@ -1,3 +1,4 @@
 resources :working_periods
 
 match '/issues/:id/toggle_pause', to: 'issues#toggle_pause', as: 'issue_toggle_pause'
+match '/issues/:id/pauses', to: 'pauses#show', as: 'issue_pauses'
