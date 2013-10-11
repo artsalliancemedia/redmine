cd165dbbb1a7d39d59ff9161e5590300095ac8cd
blrnw3 <ben.lee-rodgers@artsalliancemedia.com>
2013-10-11 13:40:35 +0100

    Start date hidden on issue form unless in Rails production mode.

--- src/plugins/aam_ui_cleanup/assets/javascripts/cleanup.js
+++ src/plugins/aam_ui_cleanup/assets/javascripts/cleanup.js
@@ -1,8 +1,11 @@
-function onLoadDoAamCleanup() {
+function onLoadDoAamCleanup(debugging) {
+	//var production = !debugging;
 	$(document).ready(function() {
-		 //console.log("Hiding unwanted UI");
 		 $("#issue_tracker_id").parent('p').hide(); //always 'support'
 		 $("#issue_due_date").parent('p').hide(); //auto-set by SLA-linked priority
 		 $("#issue_is_private").parent('p').hide(); //Don't want to give this option
+		 if(!debugging) {
+			$("#issue_start_date").parent('p').hide(); //auto-set to current datetime
+		 }
 	});
 }
