f6047d362e335dbc8152acc11f5e2f222e3e2547
Danny Butler <danny.butler@artsalliancemedia.com>
2013-10-14 17:19:28 +0100

    A few issue update changes
    Added is_paused column to issues to help with producer sync.
    Caused due date to be updated on issue save (to account for potential
    priority change).
    Got rid of Issue.all call in update_issue_due_dates methods.

--- src/plugins/aam_sla/app/controllers/working_periods_controller.rb
+++ src/plugins/aam_sla/app/controllers/working_periods_controller.rb
@@ -98,10 +98,12 @@ class WorkingPeriodsController < ApplicationController
   end
 
   def update_issue_due_dates
-    Issue.all.each do |issue|
-      unless issue.status.is_closed
-        issue.save_due_date
-      end
+    open_status_ids = []
+    IssueStatus.where('is_closed' => false).each do |status|
+      open_status_ids.push(status.id)
+    end
+    Issue.find_all_by_status_id(open_status_ids).each do |issue|
+      issue.save_due_date
     end
   end
 end
