dac569d95aa3827a845c754d6fef2d01af018433
blrnw3 <ben.lee-rodgers@artsalliancemedia.com>
2013-10-24 18:26:43 +0100

    Restore ability to create new issue priorities Allow existing priorities to have sla seconds reset to blank Fix some validation problems Prevent and warn about near-breach seconds being larger than sla seconds (or present when sla seconds is not).

--- src/plugins/aam_sla/lib/enumerations_controller_patch.rb
+++ src/plugins/aam_sla/lib/enumerations_controller_patch.rb
@@ -7,6 +7,7 @@ module EnumerationsControllerPatch
     base.send(:include, InstanceMethods)
 
     EnumerationsController.class_eval do
+		before_filter :sla_priority_grab, :only => [:new, :edit]
       after_filter :sla_seconds, :only => [:create, :update]
       after_filter :update_issue_due_dates, :only => [:create, :update, :index] # Bit of a bodge, as the updates redirect to the index page
     end
@@ -16,11 +17,25 @@ module EnumerationsControllerPatch
     def sla_seconds
       # Only save the seconds if we have a valid enumeration!
       return if not @enumeration.id or params[:seconds].nil? or params[:near_breach_seconds].nil?
-
+	  
+		if params[:near_breach_seconds].to_i > params[:seconds].to_i
+			flash.clear
+			flash[:error] = l(:bad_near_breach_seconds_warning)
+			return
+		end
+		
       sla_priority = @enumeration.sla_priority || SlaPriority.new(:issue_priority_id => @enumeration.id)
       sla_priority.seconds = params[:seconds]
-      sla_priority.near_breach_seconds = params[:near_breach_seconds]
-      sla_priority.save
+      sla_priority.near_breach_seconds = params[:near_breach_seconds].to_i #use 0 instead of null
+	  
+		sla_priority.save
+		#Need to be able to clear sla_seconds from an existing priority,
+		# so must delete its associated sla_priorty record
+		sla_priority.destroy if params[:seconds].blank? && @enumeration.sla_priority
     end
+	
+	def sla_priority_grab
+		@enumeration.build_sla_priority unless @enumeration.sla_priority
+	end
   end
 end
