c1dc061e9cb46769c874bd6ac3e67c2f14d3136e
Danny Butler <danny.butler@artsalliancemedia.com>
2013-10-24 10:25:45 +0100

    Removing out of hours string, again

--- src/plugins/aam_sla/lib/issue_patch.rb
+++ src/plugins/aam_sla/lib/issue_patch.rb
@@ -200,7 +200,7 @@ module IssuePatch
       elsif sla_status_symbol == :paused && near_breach?
         extra_string = '/' + l(:near_breach)
       end
-      l(sla_status_symbol) + extra_string + out_of_hours_string
+      l(sla_status_symbol) + extra_string
     end
 
     def css_classes_with_aam_css
